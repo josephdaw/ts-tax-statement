1f584a0b05d29ed4cac776d9a8d5d88f96be1180
josephdaw <josephdaw@icloud.com>
2023-10-29 19:37:59 +0800

    fix(lint): fixing linting

--- exchangeRateLookup.py
+++ exchangeRateLookup.py
@@ -4,24 +4,28 @@
 import pandas as pd
 # Import my local files
 
-def importRatesFromRBA(startDate, endDate):
+def importRatesFromRBA(start_date, end_date):
     print('importing rba rates...')
     # import AUDUSD from RBA interest rates - as required by the ATO
-    rbaRates1 = pd.read_excel('https://www.rba.gov.au/statistics/tables/xls-hist/2018-2022.xls',skiprows=10)
-    rbaRates2 = pd.read_excel('https://www.rba.gov.au/statistics/tables/xls-hist/2023-current.xls',skiprows=10)
-    rbaRates = pd.concat([rbaRates1, rbaRates2], axis=0)
+    rba_rates_1 = pd.read_excel(
+        'https://www.rba.gov.au/statistics/tables/xls-hist/2018-2022.xls',
+        skiprows=10)
+    rba_rates_2 = pd.read_excel(
+        'https://www.rba.gov.au/statistics/tables/xls-hist/2023-current.xls',
+        skiprows=10)
+    rba_rates = pd.concat([rba_rates_1, rba_rates_2], axis=0)
 
-    # rbaRates.to_csv('rbaRates.csv')
-    # rbaRates = pd.read_csv('rbaRates.csv')
+    # rba_rates.to_csv('rba_rates.csv')
+    # rba_rates = pd.read_csv('rba_rates.csv')
 
-    audusd = pd.DataFrame()
-    audusd['Date'] = pd.to_datetime(rbaRates['Series ID'],dayfirst=True)
-    audusd['audusd'] = rbaRates.FXRUSD
-    audusd.set_index('Date',inplace=True)
-    audusd.reset_index(inplace=True)
-    audusd.set_index('Date')  
-    audusd = audusd[audusd.Date>startDate]
-    audusd = audusd[audusd.Date<endDate]
+    aud_usd = pd.DataFrame()
+    aud_usd['Date'] = pd.to_datetime(rba_rates['Series ID'],dayfirst=True)
+    aud_usd['audusd'] = rba_rates.FXRUSD
+    aud_usd.set_index('Date',inplace=True)
+    aud_usd.reset_index(inplace=True)
+    aud_usd.set_index('Date')  
+    aud_usd = aud_usd[aud_usd.Date>start_date]
+    aud_usd = aud_usd[aud_usd.Date<end_date]
     print('...rates imported...')
 
-    return audusd
+    return aud_usd
