8fa197f4a047eda98904ff43c309adc60247c9ca
josephdaw <josephdaw@icloud.com>
2023-11-03 10:52:45 +1030

    fix(timeout): removing the time out

--- tradestationFunctions.py
+++ tradestationFunctions.py
@@ -25,7 +25,8 @@ def importOrders(startDate,endDate,accountNumber):
     }
     
     # Send a GET request to the URL and get the response content
-    response = requests.get(url, params=params, settimeout=10)
+    response = requests.get(url, params=params)
+    # response = requests.get(url, params=params, timeout=10)
     
     # print(response.text)
     
